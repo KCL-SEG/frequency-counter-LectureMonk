d23b7d8e944dfe92a49500d6ee3f93741f6b8849
William Lingard <William.Lingard@kcl.ac.uk>
2022-10-02 21:51:46 +0100

    Initial solution using presence check

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        # Presence Check
+        if (not frequencies.get(str(item))):
+            # Not Found - add item
+            frequencies[str(item)] = 1
+        else:
+            # Found - increment freq
+            frequencies[str(item)] = frequencies.get(str(item)) + 1
     return frequencies
